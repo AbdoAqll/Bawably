aa09a3bb1dd00d9f71e95134c07aa18d36c60b89
Abdelrhman Reda <11410120210225@stud.cu.edu.eg>
2025-12-29 20:23:32 +0200

    Remove UserController from DependencyContainer to streamline dependencies

--- src/Presentation/Container/DependencyContainer.h
+++ src/Presentation/Container/DependencyContainer.h
@@ -4,15 +4,12 @@
 #include <memory>
 #include <vector>
 #include <Controllers/ApartmentController/ApartmentController.h>
-
-#include "Controllers/UserController.h"
 #include "Controllers/BuildingController/BuildingController.h"
 #include "Controllers/MaintenanceRequestController/MaintenanceRequestController.h"
 #include "Controllers/RentalContractController/RentalContractController.h"
 #include "Application/UseCases/IUseCase.h"
 
 class DependencyContainer {
-    std::shared_ptr<UserController> userController;
     std::shared_ptr<BuildingController> buildingController;
     std::shared_ptr<ApartmentController> apartmentController;
     std::shared_ptr<RentalContractController> rentalContractController;
@@ -20,7 +17,6 @@ class DependencyContainer {
 
 public:
     DependencyContainer();
-    std::shared_ptr<UserController> getUserController();
     std::shared_ptr<BuildingController> getBuildingController();
     std::shared_ptr<ApartmentController> getApartmentController();
     std::shared_ptr<RentalContractController> getRentalContractController();
